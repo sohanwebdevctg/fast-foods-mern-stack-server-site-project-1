47db2ad0c2ccd79c27d68cc7f492013f42c0b19f
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-09 23:34:26 +0600

    adding userCart delete data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleware
@@ -53,6 +53,14 @@ async function run() {
       }
       const result = await userCartsCollections.find(query).toArray();
       res.send(result)
+    })
+
+    //delete user cart data
+    app.delete('/userCarts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCartsCollections.deleteOne(query);
+      res.send(result);
     })
 
 
